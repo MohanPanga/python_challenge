712e37ce293f2957868501290955e34ac56d5910
Mohan Panga <mohanpanga@Mohans-Mac-mini.local>
2021-05-04 21:42:34 -0500

    initial draft of PyPoll

---
+++ PyPoll/main.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+csvpath = os.path.join("Resources","election_data.csv")
+print(csvpath)
+
+with open(csvpath,'r') as csvhandle:
+    election_data = csv.reader(csvhandle)
+    header = next(election_data)
+    Total_votes = 0
+    candidates =[]
+    candidate_votes =[]
+
+    for row in election_data:
+        Total_votes+= 1
+        try:
+            indx = candidates.index(row[2])
+        except ValueError:
+            candidates.append(row[2])
+            candidate_votes.append(1)
+        else:
+            candidate_votes[indx] += 1
+    
+print('Election Results')
+print('---------------------------------------------')
+print(f'Total Votes: {Total_votes}')
+print(candidates)
+print(candidate_votes)
+
